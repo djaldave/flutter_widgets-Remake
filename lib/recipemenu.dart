e9c87a0e46201131047158f6988306b01400156e
djaldave <djaldave@gmail.com>
2020-10-18 03:21:34 +0800

    add color and padding

--- lib/recipemenu.dart
+++ lib/recipemenu.dart
@@ -4,22 +4,35 @@ class RecipeMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Column(
           children: [
-            IconButton(icon: Icon(Icons.favorite), onPressed: () {}),
+            IconButton(
+              icon: Icon(Icons.favorite),
+              onPressed: () {},
+              color: Colors.red,
+            ),
             Text("Favorites"),
           ],
         ),
+        Padding(padding: EdgeInsets.only(left: 15)),
         Column(
           children: [
-            IconButton(icon: Icon(Icons.create), onPressed: () {}),
+            IconButton(
+                icon: Icon(Icons.create),
+                onPressed: () {},
+                color: Colors.green),
             Text("My Recipes"),
           ],
         ),
+        Padding(padding: EdgeInsets.only(left: 15)),
         Column(
           children: [
-            IconButton(icon: Icon(Icons.image_search), onPressed: () {}),
+            IconButton(
+                icon: Icon(Icons.image_search),
+                onPressed: () {},
+                color: Colors.orange),
             Text("My Recipes"),
           ],
         ),
