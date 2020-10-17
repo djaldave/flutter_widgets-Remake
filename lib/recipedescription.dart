f3d0ead1a1f0aa2c608b4ff21924828bb73c7ce1
djaldave <djaldave@gmail.com>
2020-10-18 02:14:38 +0800

    row and mainAxisAlignment center

--- lib/recipedescription.dart
+++ lib/recipedescription.dart
@@ -3,11 +3,27 @@ import 'package:flutter/material.dart';
 class RecipeDescription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Fried Chicken"),
-        Text("by Cookmaster"),
-      ],
+    return Center(
+      child: Column(
+        children: [
+          Text("Fried Chicken"),
+          Text("by Cookmaster"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Cooking Time: "),
+              Text("20 min"),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Category: "),
+              Text("Main Dish"),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
