7941181a4128522e51c1412cc43e5daacb9fc5fd
djaldave <djaldave@gmail.com>
2020-10-18 04:50:21 +0800

    TextFormField

--- lib/registrationform.dart
+++ lib/registrationform.dart
@@ -5,14 +5,36 @@ class RegistrationForm extends StatefulWidget {
   _RegistrationFormState createState() => _RegistrationFormState();
 }
 
+InputDecoration txtDecoration(hintText) {
+  return InputDecoration(
+    fillColor: Colors.white,
+    filled: true,
+    hintText: hintText,
+  );
+}
+
 class _RegistrationFormState extends State<RegistrationForm> {
   final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: Column(
-        children: [],
+        children: [
+          TextFormField(decoration: txtDecoration('First Name')),
+          TextFormField(decoration: txtDecoration('Last Name')),
+          TextFormField(decoration: txtDecoration('Username')),
+          TextFormField(decoration: txtDecoration('Email')),
+          TextFormField(
+            decoration: txtDecoration('Password'),
+            obscureText: true,
+          ),
+          TextFormField(
+            decoration: txtDecoration('Confirm Password'),
+            obscureText: true,
+          ),
+        ],
       ),
     );
   }
